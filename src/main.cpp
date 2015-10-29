2247d6fe5c2aa1d8d93f7aabbe6c60074a6eaf25
Lionel Koenig <lionel.koenig@gmail.com>
2015-10-29 08:39:10 +0100

    Little bit of framework

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,40 @@ int main(int argc, char **argv) {
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
         640, 480, 
         SDL_WINDOW_SHOWN);
+
+    //The surface contained by the window SDL_Surface* screenSurface = NULL;
+    SDL_Surface* screenSurface = NULL;
+    
     PhysicsEngine *physics = new PhysicsEngine();
     Vessel *vessel = new Vessel();
 
     physics->add_body(vessel);
     double time_delta = .1; // 0.1 should be sufficient
-
-    for (double t = 0.0; t < 1800.; t += time_delta) {
+    bool done=false;
+    double t=0;
+    
+    while(!done){
+        // Handling event
+        SDL_Event evt;
+        while(SDL_PollEvent(&evt))
+        {
+            if(evt.type==SDL_QUIT)
+                done=true;
+        }
+        
+        // Physic simulation
         physics->timestep(time_delta);
         physics->print_all_positions();
+        
+        
+        // Rendering
+        screenSurface = SDL_GetWindowSurface( window );
+        SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
+        SDL_UpdateWindowSurface( window );
+
+        // Time increase
+        done |= t > 1800.;
+        t+=time_delta;
     }
 
     delete vessel;
